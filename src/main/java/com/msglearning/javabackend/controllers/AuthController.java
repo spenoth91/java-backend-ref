04a9a276ac12ef3eeb59a76d81db587927e6fe50
Botond <laszlo.botond1991@gmail.com>
2022-05-25 18:26:22 +0300

    On login, send email pw in header

--- src/main/java/com/msglearning/javabackend/controllers/AuthController.java
+++ src/main/java/com/msglearning/javabackend/controllers/AuthController.java
@@ -5,10 +5,12 @@ import com.msglearning.javabackend.services.PasswordService;
 import com.msglearning.javabackend.services.Tokenservice;
 import com.msglearning.javabackend.services.UserService;
 import com.msglearning.javabackend.to.UserTO;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.Set;
@@ -19,6 +21,7 @@ public class AuthController {
 
     private static final String REGISTER_PATH = "/register";
     private static final String LOGIN_PATH = "/login";
+    public static final String AUTHORIZATION = "authorization";
 
     @Autowired
     UserService userService;
@@ -39,17 +42,21 @@ public class AuthController {
     }
 
     @PostMapping(LOGIN_PATH)
-    public String login(@RequestBody UserTO userTO) {
+    public String login(@RequestHeader Map<String, String> headers) {
 
-        if ( !StringUtils.hasLength(userTO.getEmail()) || !StringUtils.hasLength(userTO.getPassword())) {
+        String decodedAuthHeader = new String(Base64.decodeBase64(headers.get(AUTHORIZATION).substring(6)));
+        String email = decodedAuthHeader.substring(0, decodedAuthHeader.indexOf(":"));
+        String pw = decodedAuthHeader.substring(decodedAuthHeader.indexOf(":") + 1);
+
+        if ( !StringUtils.hasLength(email) || !StringUtils.hasLength(pw)) {
 
             //No email and/or password
 
             return "Forbidden";
         }
 
-        Optional<User> userOpt = this.userService.findByEmail(userTO.getEmail());
-        if (userOpt.isPresent() && PasswordService.checkPassword(userTO.getPassword(), userOpt.get().getPassword())) {
+        Optional<User> userOpt = this.userService.findByEmail(email);
+        if (userOpt.isPresent() && PasswordService.checkPassword(pw, userOpt.get().getPassword())) {
 
             // Create token
 
